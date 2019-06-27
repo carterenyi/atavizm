5e8ed457b36540d585bd65509ada28b3f515b326
wsdula <wsdula@gmail.com>
2019-06-27 16:01:54 -0400

    Updates from over the course of 2018-2019

--- Arc/midi2nmat.py
+++ Arc/midi2nmat.py
@@ -1,34 +1,38 @@
 import pandas as pd
 import mido
 
-def midi2nmat(path: str):
-    """Converts MIDI file from raw messages to Note Matrix format"""
+import pandas as pd
+import mido
+
+def midi2nmat(path):
+    """Returns a Note Matrix from a given MIDI files"""
+    
+    assert (type(path) is str), "Filepath must be a string: %r" % path
     mid = mido.MidiFile(path)
     tpb = mid.ticks_per_beat
 
-    midiframe = pd.DataFrame(columns=["Voice", "Pitch", "Velocity", "Ticks"])
+    midiframe = pd.DataFrame(columns=["Type", "Voice", "Pitch", "Velocity", "Ticks"])
 
     for track in mid.tracks:
         for msg in track:
-            if msg.type == 'note_on':
-                df = pd.DataFrame({"Voice":msg.channel, "Pitch":msg.note, "Velocity":msg.velocity, "Ticks":msg.time},\
-                                  index=[0])
-                midiframe = midiframe.append(df, ignore_index=True)
+            if msg.type == "note_on":
+                df = pd.DataFrame({"Type":msg.type, "Voice":msg.channel, "Pitch":msg.note, "Velocity":msg.velocity, "Ticks":msg.time},\
+                              index=[0])
+            else:
+                df = pd.DataFrame({"Type":msg.type, "Ticks":msg.time},\
+                              index=[0])
+            midiframe = midiframe.append(df, ignore_index=True)
 
-    midiframe.loc[0,'Ticks'] = 0
+    midiframe["Time"] = pd.Series(midiframe["Ticks"].cumsum() / tpb, index=midiframe.index)
 
-    midiframe['Time'] = pd.Series(midiframe['Ticks'].cumsum()/tpb, index=midiframe.index)
-
-    note_on = pd.DataFrame(midiframe[midiframe['Velocity'] != 0])
-    note_off = pd.DataFrame(midiframe[midiframe['Velocity'] == 0])
+    note_on = midiframe.loc[(midiframe["Velocity"] != 0) & (midiframe["Type"] == "note_on")]
+    note_off = midiframe.loc[(midiframe["Velocity"] == 0) & (midiframe["Type"] == "note_on")]
     newdex = range(0,len(note_off))
 
     note_off = note_off.reset_index(drop=True)
     note_on = note_on.reset_index(drop=True)
 
-    note_on['Duration'] = pd.Series(note_off['Time'] - note_on['Time'], index = note_on.index)
+    note_on["Duration"] = pd.Series(note_off["Time"] - note_on["Time"], index = note_on.index)
 
-    return note_on[['Pitch', 'Time', 'Velocity', 'Voice', 'Duration']]
-
-def _nmatPloy2Mono(nmat):
-    """Turns Polyphonic MIDI File into Monophonic MIDI file"""
+    return note_on[["Pitch", "Time", "Velocity", "Voice", "Duration"]]
+    
